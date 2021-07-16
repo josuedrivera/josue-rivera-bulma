3649e142659955480363376c33c4c960c4352488
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-07-15 23:44:53 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 console.log("connected");
+
+
+// COPY EMAIL IN CONTACT SECTION
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+// ^^^REWRITE IN VANILLA JS
+// function myFunction() {
+//     var copyText = document.getElementById("myemail");
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999);
+//     document.execCommand("copy");
+    
+//     var tooltip = document.getElementById("myTooltip");
+//     tooltip.innerHTML = "Copied: " + copyText.value;
+//   }
+  
+//   function outFunc() {
+//     var tooltip = document.getElementById("myTooltip");
+//     tooltip.innerHTML = "Copy to clipboard";
+//   }

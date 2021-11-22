736b0203c4f080cc001061f65ec0ce67b84da1e1
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-11-21 17:26:31 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,3 +63,114 @@ const three = {
 		</div>
 	`
 }
+
+
+const headshot = {
+	template: `
+  
+	`
+}
+const aboutMe = {
+	template: `
+    <div class="container is-fluid has-background-grey-darker has-text-danger py-4 is-flex is-flex-direction-column is-align-items-space-between">
+        <p class="title block has-text-primary pb-3">Ars Longa Vita Brevis</p>
+
+        <p class="subtitle block has-text-primary-dark pb-0"> Hello World! My name is 
+        <span class="is-size-4 has-text-weight-bold">Josue</span>. 
+        If you can't say that, <a class="has-text-primary has-text-weight-bold" href="https://www.google.com/search?q=google+pronounce+josue&rlz=1C5CHFA_enUS728US728&oq=google+pronounce+josue&aqs=chrome..69i57j0i333j69i64.5107j1j7&sourceid=chrome&ie=UTF-8"> try this</a> 
+        then amuse me *sarcasm*, or just call me Sway. I am a 
+        <span class="has-text-weight-bold"> Front-End Developer</span> from the beautiful 
+        <span class="has-text-weight-bold"> Bay Area, California</span>.</p>
+
+        <p class="block has-text-primary-dark"> As a Web Developer, I enjoy the challenge for creative solutions, and the demand for curiosity, critical thinking, and efficient code required to produce content worthy of an audience. I am passionate about building as well as deconstructing 'things' while learning throughout the whole process. <br><br>
+        
+        I am also familiar with many aspects of Back-End Development, UX/UI, and Content Management Systems. I am currently utilizing these skills with the ultimate goal of joining a team that enjoys solving difficult problems, shares in my appreciation for direct, clear, and concise communication and open-minded feedback.</p>
+
+        <div class="">
+        <p class="block is-size-6 has-text-right"> 
+        learn more about me 
+        <i class="fas fa-angle-down welcome-down p-0 m-0"></i>	
+        </p>
+        </div>
+    </div>
+	`
+}
+const hardSkills = {
+	template: `
+    <div class="container is-fluid has-background-grey-darker has-text-danger py-4">
+        <p class="title block has-text-primary">Hard Skills</p>
+
+        <span class="block subtitle has-text-primary has-text-weight-bold"> Languages:  <br></span>
+        <div class="block is-flex is-flex-direction-row">
+        <p class="subtitle block has-text-primary-dark">
+        <i class="fab fa-html5 is-size-1 mt-3 mx-3"></i> |HTML5 
+        <i class="fab fa-css3-alt is-size-1 mt-3 mx-3"></i> |CSS3 
+        <i class="fab fa-js-square is-size-1 mt-3 mx-3"></i> |JAVASCRIPT (ES5/ES6)
+        </p>
+        </div>
+        
+        <span class="block subtitle has-text-primary has-text-weight-bold"> Applications:  <br></span>
+        <p class="subtitle block has-text-primary-dark">
+        <span class="">WordPress, SquareSpace, SharePoint, Shopify <br></span>
+        <span class="">SCSS/SASS, Bootstrap,  Bulma, Semantic UI <br></span>
+        <span class="">Vue.js, jQuery, Node.js, NPM +a variety of node packages <br></span>
+        </p>
+
+        <p><span class="has-text-weight-bold"> Design Process & Theory</span> | Responsive Design, Media Queries, Version Control, Github, AJAX, JSON, APIs, RESTful Routes, CSS Naming Convention, Cross Browser Development, Browser Dev Tools, Testing/Debugging, Command Line... </p><br>
+
+        <ul class="block is-size-7"> 
+            <span class="has-text-weight-bold"> Also experienced with: </span>
+            <li>Adobe XD, Adobe CC(Illustrator, Photoshop, InDesign)</li>
+            <li>MS Suite, Google Suite (including Apps Script)</li>
+            <li>Express.js, passport.js, and various Node.js packages</li>
+            <li>JIRA - Issue and Project Tracking Software, others, and still eagerly learning....</li>
+        </ul>				
+    </div>
+	`
+}
+const softSkills = {
+	template: `
+    <div class="container is-fluid has-background-grey-darker has-text-danger py-4">
+        <p class="title block has-text-primary pb-3">Soft Skills</p>
+        
+        <p class="subtitle block has-text-primary-dark pb-2">
+          As introduced by The 4 Agreements, I subscribe to the idea that all humans are artists and life is our canvas. Seeing the world through this filter, I am excited and intrigued by others' creations. I respect their style and encourage their best because life has shown me that when people feel safe, happy, and motivated, awesome things are created!
+        </p>
+
+        
+        <ul class="block has-text-right"> <span class="is-size-4 has-text-weight-bold"> Core Competencies: </span>
+            <li> Naturally Creative • Conceptual/Critical Thinker and Tinkerer </li>
+            <li> Adaptable • Solution Oriented • Problem Solver w/a Drive for Efficiency</li>
+            <li> Embraces Teamwork • Empathic and Open Minded • Empowering of Others </li>
+            <li> Accountable & Approachable • Ability To Take Criticism • Learns From Mistakes </li>
+            <li> Interpersonal Awareness • Willingness to Grow • Self-Starter/Self-Motivated </li>
+            <li> Excellent Written and Verbal Communication Skills and ability to follow process</li>
+        </ul>  
+
+        <p class="has-text-primary has-text-centered is-size-7">thnx for coming to my TedTalk, check out my --blog in progress--</p>
+    </div>
+	`
+}
+const spaceMan = {
+  template: `
+  <div class="has-text-left m-0 has-background-black-ter rotating-text-box">
+    <!-- ROTATING TEXT -->
+    <div class="rotating-text has-background-black-ter m-0 p-0">
+    
+      <div class=" is-inline-block">
+        <h2 class="title is-size-1 has-text-warning">SOME OF MY FAVORITE THINGS:</h2>
+      </div>
+   
+      <div class=" is-inline-block">
+        <p class="">
+          <span class="word alizarin">tacos.</span>
+          <span class="word wisteria">football.</span>
+          <span class="word peter-river">napping.</span>
+          <span class="word emerald">hanging_out.</span>
+          <span class="word sun-flower">free_stuff.</span>
+        </p>
+      </div>
+    </div>
+  </div>
+  `
+}

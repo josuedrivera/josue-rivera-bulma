c7b307771e0b15ac9a0a0f49be9742f722176da1
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-11-29 23:52:30 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -348,3 +348,45 @@ var projects = new Vue({
 //     </transition>
 // 	`
 // }
+
+// const appCarousel = new Vue({
+// 	el: '#appCarousel',
+// 	data() {
+//         return {
+//             slides: [
+//                 'https://unsplash.com/photos/eoHUaRLrYxw',
+//                 'https://unsplash.com/photos/BHJs5TZ-Nt0',
+//                 'https://unsplash.com/photos/FilM6ng7VGQ'
+//             ],
+//             visibleSlide : 0,
+//             direction: 'left',
+//         }
+//     },
+// 	computed: {
+//         sLength() {
+//             return this.slides.length;
+//         }
+//     },
+//     methods: {
+//         next()  {
+//             if(this.visibleSlide >= this.sLength - 1) {
+//                 this.visibleSlide = 0;
+//             } else {
+//                 this.visibleSlide++;
+//             }
+//             this.direction = "left"
+//         },
+//         prev()  {
+//             if(this.visibleSlide <= 0) {
+//                 this.visibleSlide = this.sLength - 1;
+//             } else {
+//                 this.visibleSlide--;
+//             }
+//             this.direction = "right"
+//         }
+//     }, 
+//     components : {
+//         Carousel,
+//         Slide,
+//     }
+// }).$mount('#appCarousel')

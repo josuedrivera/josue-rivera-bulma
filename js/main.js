638958d0ddac6235d903d19197214c3aa2e81901
Josue Rivera <41710630+josuedrivera@users.noreply.github.com>
2021-12-08 23:52:21 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -470,4 +470,28 @@ var projects = new Vue({
 // });
 
 
+// STICKY NAVBAR
+// When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// Get the navbar
+// var navbar = document.getElementById("sticky-navbar");
+// var welcomenav = document.getElementById("welcome-nav");
+
+// Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//     welcomenav.classList.add("hide");
+//   } else {
+//     navbar.classList.remove("sticky");
+//     welcomenav.classList.remove("hide");
+//   }
+// }
+
+
+
 
